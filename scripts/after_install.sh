13e5f7accaa959e9f9986cf8dae67a97f2415260
AVANI-GAJJAR <78599804+AVANI-GAJJAR@users.noreply.github.com>
2024-01-30 16:49:34 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 
-# clean codedeploy-agent files for a fresh install
-sudo rm -rf /home/ubuntu/install
+# kill any servers that may be running in the background 
+sudo pkill -f runserver
 
-# install CodeDeploy agent
-sudo apt-get -y update
-sudo apt-get -y install ruby
-sudo apt-get -y install wget
-cd /home/ubuntu
-wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
-sudo chmod +x ./install 
-sudo ./install auto
+# kill frontend servers if you are deploying any frontend
+# sudo pkill -f tailwind
+# sudo pkill -f node
 
-# update os & install python3
-sudo apt-get update
-sudo apt-get install -y python3 python3-dev python3-pip python3-venv
-pip install --user --upgrade virtualenv
+cd /home/ubuntu/Fake_Profile_Detection/
 
-# delete app
-sudo rm -rf /home/ubuntu/Fake_Profile_Detection
+# activate virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
+# install requirements.txt
+pip install -r /home/ubuntu/Fake_Profile_Detection/requirements.txt
+
+# run server
+screen -d -m python3 manage.py runserver 0:8000
