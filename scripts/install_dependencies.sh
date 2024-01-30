810d29619b12beab9f594f7a49bd29b817c06492
AVANI-GAJJAR <78599804+AVANI-GAJJAR@users.noreply.github.com>
2024-01-30 15:09:58 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt install -y python3-pip
+sudo apt install -y virtualenv
 virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
 pip install -r /home/ubuntu/Fake_Profile_Detection/requirements.txt
