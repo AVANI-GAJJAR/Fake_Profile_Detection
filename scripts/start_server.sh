2617fde52c82184e0566fb4d2d736b4f7869ee66
AVANI-GAJJAR <78599804+AVANI-GAJJAR@users.noreply.github.com>
2024-01-30 15:20:07 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-cd /home/ubuntu/Fake_Profile_Detection
-python manage.py runserver localhost:8000
+sed -i 's/\[]/\["54.175.190.186"]/' /home/ubuntu/Fake_Profile_Detection/Fake_Profile_Detection/settings.py
+
+python manage.py migrate 
+python manage.py makemigrations     
+python manage.py collectstatic
+python manage.py runserver 
 chmod +x /home/ubuntu/Fake_Profile_Detection/scripts/start_server.sh
